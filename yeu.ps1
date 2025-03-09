dd4a634fd40527e87669075363509acd1af7ffbd
kdzvcllovebfa <kiem161210@gmail.com>
2025-03-09 23:16:14 +0700

    Update yeu.ps1

--- yeu.ps1
+++ yeu.ps1
@@ -1,15 +1,1 @@
-$Text = "💖 YEU BUI PHUONG ANH 💖"
-$MaxMove = 20  # Độ dài di chuyển
-$Direction = 1 # 1 = đi sang phải, -1 = đi sang trái
-$Pos = 0
 
-while ($true) {
-    Clear-Host
-    $Output = (" " * $Pos) + $Text
-    Write-Host $Output
-    Start-Sleep -Milliseconds 100  # Điều chỉnh tốc độ di chuyển
-
-    # Đổi hướng khi chạm biên
-    $Pos += $Direction
-    if ($Pos -ge $MaxMove -or $Pos -le 0) { $Direction *= -1 }
-}
